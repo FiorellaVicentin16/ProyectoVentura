923a9cd93f3d88203b2ddfc95d39a1aa1521e989
FiorellaVicentin16 <fiorellavicentin@gmail.com>
2016-10-07 12:43:04 -0300

    Nuevo

---
+++ Ventura/resources/views/Inicio/bienvenidos.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/../css/fondo.css">
+  <link rel="stylesheet" href="../css/animate.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>	
+  <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> 
+  <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+	<body>
+
+
+
+   <img src="../images/Home.png">  
+ 
+	</body>
+
+</html>
