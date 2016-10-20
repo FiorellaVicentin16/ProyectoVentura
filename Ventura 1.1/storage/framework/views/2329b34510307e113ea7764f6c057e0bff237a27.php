091cd0aea9258749360c25a59f12ac6c73c7c50b
FiorellaVicentin16 <fiorellavicentin@gmail.com>
2016-10-20 07:13:52 -0300

    versionado

---
+++ Ventura 1.1/storage/framework/views/2329b34510307e113ea7764f6c057e0bff237a27.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+  <div class="bgimg-1">
+  <?php $__env->stopSection(); ?>
+
+
+
+
+  
+
+  
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
